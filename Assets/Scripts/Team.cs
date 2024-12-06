115c2acd66c24fd473824e20e88af1b617bc678f
RENNES\game <148343059+BlondeauLouis@users.noreply.github.com>
2024-12-06 14:35:54 +0100

    affichage de la Team

--- Assets/Scripts/Team.cs
+++ Assets/Scripts/Team.cs
@@ -1,27 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Team : MonoBehaviour
 {
     public Spawner spawner;
     public PokemonSE[] members = new PokemonSE[3];
+
+    [SerializeField]
+    private Image _pokemon0Sprite;
+    [SerializeField]
+    private Image _pokemon1Sprite;
+    [SerializeField]
+    private Image _pokemon2Sprite;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(members[0]);
         if (members[0] != null)
         {
+            _pokemon0Sprite.sprite = members[0].sprite;
             StartCoroutine(AutoClick0());
         }
 
         if (members[1] != null)
         {
+            _pokemon1Sprite.sprite = members[1].sprite;
             StartCoroutine(AutoClick1());
         }
 
         if (members[2] != null)
         {
+            _pokemon2Sprite.sprite = members[2].sprite;
             StartCoroutine(AutoClick2());
         }
     }
@@ -34,20 +44,30 @@ public class Team : MonoBehaviour
 
     private IEnumerator AutoClick0()
     {
-        while (true)
+        while (members[0]!=null)
         {
-            spawner.ReduceHp(10);
-            yield return new WaitForSeconds(2);
+            spawner.ReduceHp(members[0].atq);
+            yield return new WaitForSeconds(members[0].cooldown);
         }
+        yield break;
     }
 
     private IEnumerator AutoClick1()
     {
-        yield return new WaitForSeconds(members[2].cooldown);
+        while (true)
+        {
+            spawner.ReduceHp(members[1].atq);
+            yield return new WaitForSeconds(members[1].cooldown);
+        }
+
     }
 
     private IEnumerator AutoClick2()
     {
-        yield return new WaitForSeconds(members[3].cooldown);
+        while (true)
+        {
+            spawner.ReduceHp(members[2].atq);
+            yield return new WaitForSeconds(members[2].cooldown);
+        }
     }
 }
