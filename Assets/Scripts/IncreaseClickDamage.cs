048c05fbebdfbe952348551122911256466a2871
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2024-12-12 17:24:59 +0100

    1er objet terminado finito pipo

--- Assets/Scripts/IncreaseClickDamage.cs
+++ Assets/Scripts/IncreaseClickDamage.cs
@@ -3,38 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.UI;
 using TMPro;
+using UnityEngine.EventSystems;
+using UnityEditor.PackageManager;
 
-public class IncreaseClickDamage : MonoBehaviour
+public class IncreaseClickDamage : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
 
     public GameManager manager;
 
-    public TextMeshProUGUI priceText, powerText;
+    public TextMeshProUGUI priceText;
 
     [SerializeField]
     private int _price, _power;
+    [SerializeField]
+    private TextMeshProUGUI _infos, _noMoney;
     // Start is called before the first frame update
     void Start()
     {
         _price = 15;
         _power = 1;
+        _infos.enabled = false;
+        _noMoney.enabled = false;
+        _infos.text = "Achetez un Bracelet Macho pour augmenter de " + _power + " la puissance de votre clic (puissance actuelle : " + manager.clickDamage + ")";
     }
 
     // Update is called once per frame
     void Update()
     {
         priceText.text = _price.ToString() + " $";
-        powerText.text = _power.ToString();
+        Debug.Log(_infos.text);
     }
 
     public void Purchase()
     {
-        if (manager.pokedollars >= _price) 
-        { 
+        if (manager.pokedollars >= _price)
+        {
             manager.pokedollars -= _price;
             manager.clickDamage += _power;
-            _power ++;
+            _power++;
             _price *= 2;
+            _infos.text = "Achetez un Bracelet Macho pour augmenter de " + _power + " la puissance de votre clic (puissance actuelle : " + manager.clickDamage + ")";
         }
+        else 
+        {
+            StartCoroutine(NoMoneyMessage());
+        }
+    }
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("oui");
+        //new System.NotImplementedException();
+        _infos.enabled = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        //new System.NotImplementedException();
+        _infos.enabled = false;
+    }
+    private IEnumerator NoMoneyMessage()
+    {
+        _noMoney.enabled = true;
+        yield return new WaitForSeconds(2);
+        _noMoney.enabled = false;
     }
 }
