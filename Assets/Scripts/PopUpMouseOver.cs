aa0df738772d7923ac5879aa9a46602c796ac97f
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-01-07 08:33:18 +0100

    ENFIN CORRIGÉ

--- Assets/Scripts/PopUpMouseOver.cs
+++ Assets/Scripts/PopUpMouseOver.cs
@@ -21,17 +21,52 @@ public class PopUpMouseOver : MonoBehaviour, IPointerEnterHandler, IPointerExitH
     // Update is called once per frame
     void Update()
     {
-        if (place == 0)
+        if (team.members[0] != null)
         {
-            if (team.members[0].type2 != PokemonType.None)
+            if (place == 0)
             {
-                _infos.text = team.members[0].pokemonName + "\n" + team.members[0].type1 + " " + team.members[0].type2 + "\nAttaque : " + team.atk0.ToString() + "\nCooldown : " + team.speed0.ToString();
-            }
-            else
-            {
-                _infos.text = team.members[0].pokemonName + "\n" + team.members[0].type1 + "\nAttaque : " + team.atk0.ToString() + "\nCooldown : " + team.speed0.ToString();
-            }
+                if (team.members[0].type2 != PokemonType.None)
+                {
+                    _infos.text = team.members[0].pokemonName + "\n" + team.members[0].type1 + " " + team.members[0].type2 + "\nAttaque : " + team.atk0.ToString() + "\nCooldown : " + team.speed0.ToString();
+                }
+                else
+                {
+                    _infos.text = team.members[0].pokemonName + "\n" + team.members[0].type1 + "\nAttaque : " + team.atk0.ToString() + "\nCooldown : " + team.speed0.ToString();
+                }
 
+            }
+        }
+
+        if (team.members[1] != null)
+        {
+            if (place == 1)
+            {
+                if (team.members[1].type2 != PokemonType.None)
+                {
+                    _infos.text = team.members[1].pokemonName + "\n" + team.members[1].type1 + " " + team.members[1].type2 + "\nAttaque : " + team.atk1.ToString() + "\nCooldown : " + team.speed1.ToString();
+                }
+                else
+                {
+                    _infos.text = team.members[1].pokemonName + "\n" + team.members[1].type1 + "\nAttaque : " + team.atk1.ToString() + "\nCooldown : " + team.speed1.ToString();
+                }
+
+            }
+        }
+
+        if (team.members[2] != null)
+        {
+            if (place == 2)
+            {
+                if (team.members[2].type2 != PokemonType.None)
+                {
+                    _infos.text = team.members[2].pokemonName + "\n" + team.members[2].type1 + " " + team.members[2].type2 + "\nAttaque : " + team.atk2.ToString() + "\nCooldown : " + team.speed2.ToString();
+                }
+                else
+                {
+                    _infos.text = team.members[2].pokemonName + "\n" + team.members[2].type1 + "\nAttaque : " + team.atk2.ToString() + "\nCooldown : " + team.speed2.ToString();
+                }
+
+            }
         }
     }
 
