de58c10d5b02d9c3d0adc7866cc994db5552194e
BlondeauLouis <148343059+BlondeauLouis@users.noreply.github.com>
2025-01-08 22:34:39 +0100

    envie de caner
    on revient au système de save d'origine tout nul là

--- Assets/Scripts/SaveLoad.cs
+++ Assets/Scripts/SaveLoad.cs
@@ -16,26 +16,26 @@ public class SaveLoad : MonoBehaviour
         PlayerPrefs.SetInt("ClickDamage", manager.clickDamage);
         PlayerPrefs.SetInt("Level", manager.level);
 
-        if (team.members[0] != null)
-        {
-            PlayerPrefs.SetString("Team0", team.members[0].pokemonName);
-            PlayerPrefs.SetInt("Atk0", team.atk0);
-            PlayerPrefs.SetFloat("Speed0", team.speed0);
-        }
+        //if (team.members[0] != null)
+        //{
+        //    PlayerPrefs.SetString("Team0", team.members[0].pokemonName);
+        //    PlayerPrefs.SetInt("Atk0", team.atk0);
+        //    PlayerPrefs.SetFloat("Speed0", team.speed0);
+        //}
 
-        if (team.members[1] != null)
-        {
-            PlayerPrefs.SetString("Team1", team.members[1].pokemonName);
-            PlayerPrefs.SetInt("Atk1", team.atk1);
-            PlayerPrefs.SetFloat("Speed1", team.speed1);
-        }
+        //if (team.members[1] != null)
+        //{
+        //    PlayerPrefs.SetString("Team1", team.members[1].pokemonName);
+        //    PlayerPrefs.SetInt("Atk1", team.atk1);
+        //    PlayerPrefs.SetFloat("Speed1", team.speed1);
+        //}
 
-        if (team.members[2] != null)
-        {
-            PlayerPrefs.SetString("Team2", team.members[2].pokemonName);
-            PlayerPrefs.SetInt("Atk2", team.atk2);
-            PlayerPrefs.SetFloat("Speed2", team.speed2);
-        }
+        //if (team.members[2] != null)
+        //{
+        //    PlayerPrefs.SetString("Team2", team.members[2].pokemonName);
+        //    PlayerPrefs.SetInt("Atk2", team.atk2);
+        //    PlayerPrefs.SetFloat("Speed2", team.speed2);
+        //}
 
     }
 
@@ -46,27 +46,27 @@ public class SaveLoad : MonoBehaviour
         manager.clickDamage = PlayerPrefs.GetInt("ClickDamage");
         manager.level = PlayerPrefs.GetInt("Level");
 
-        for (int i = 0; i < spawner.encounters.Length; i++)
-        {
-            PokemonSE pokemon = spawner.encounters[i];
-            if (pokemon.pokemonName == PlayerPrefs.GetString("Team0"))
-            {
-                team.members[0] = pokemon;
-                team.atk0 = PlayerPrefs.GetInt("Atk0");
-                team.speed0 = PlayerPrefs.GetFloat("Speed0");
-            }
-            if (pokemon.pokemonName == PlayerPrefs.GetString("Team1"))
-            {
-                team.members[1] = pokemon;
-                team.atk1 = PlayerPrefs.GetInt("Atk1");
-                team.speed1 = PlayerPrefs.GetFloat("Speed1");
-            }
-            if (pokemon.pokemonName == PlayerPrefs.GetString("Team2"))
-            {
-                team.members[2] = pokemon;
-                team.atk2 = PlayerPrefs.GetInt("Atk2");
-                team.speed2 = PlayerPrefs.GetFloat("Speed2");
-            }
-        }
+        //for (int i = 0; i < spawner.encounters.Length; i++)
+        //{
+        //    PokemonSE pokemon = spawner.encounters[i];
+        //    if (pokemon.pokemonName == PlayerPrefs.GetString("Team0"))
+        //    {
+        //        team.members[0] = pokemon;
+        //        team.atk0 = PlayerPrefs.GetInt("Atk0");
+        //        team.speed0 = PlayerPrefs.GetFloat("Speed0");
+        //    }
+        //    if (pokemon.pokemonName == PlayerPrefs.GetString("Team1"))
+        //    {
+        //        team.members[1] = pokemon;
+        //        team.atk1 = PlayerPrefs.GetInt("Atk1");
+        //        team.speed1 = PlayerPrefs.GetFloat("Speed1");
+        //    }
+        //    if (pokemon.pokemonName == PlayerPrefs.GetString("Team2"))
+        //    {
+        //        team.members[2] = pokemon;
+        //        team.atk2 = PlayerPrefs.GetInt("Atk2");
+        //        team.speed2 = PlayerPrefs.GetFloat("Speed2");
+        //    }
+        //}
     }
 }
